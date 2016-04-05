9649cf131b7c1d6ccf37ae6059411d20a311b273
Jason Noxon <jason@palindromecreative.com>
2016-04-05 13:50:58 -0400

    Fixed DELETE function and API call

--- js/chatter-web.js
+++ js/chatter-web.js
@@ -17,6 +17,36 @@ function ViewModel() {
 
 	}
 
+	self.Add = function() {
+		
+		$.ajax({
+			url: 'http://chatter-api.azurewebsites.net/api/Chit', 
+			method: 'post',
+			data: "[{ 'UserName': 'Jason', 'Message': 'Delete Me, Please!' }]",
+			success: function(data){
+				self.Init();
+				
+			}
+
+		});
+
+	}
+
+	self.Delete = function(chit) {
+		$.ajax({
+			url: 'http://chatter-api.azurewebsites.net/api/Chit/' + chit.Id, 
+			method: 'delete',
+			success: function(data) {
+				self.Init();
+			},
+			error: function(data) {
+				console.error(data);
+			}
+		});
+
+	
+	}
+
 	self.Init();
 }
 
